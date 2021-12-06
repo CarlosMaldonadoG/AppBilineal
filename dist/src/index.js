8c2db2d17a44ce5bc736a97034c07b506a0888a8
CarlosMaldonadoG <buddylxp97@gmail.com>
2021-12-05 22:56:05 -0600

    Modificaciones al proyecto

--- dist/src/index.js
+++ dist/src/index.js
@@ -6,10 +6,10 @@ var lienzo1;
 var lienzo2;
 var pantalla1;
 var pantalla2;
-var arr_Img = new Array(); //contador donde guardo los estados de la imagen para poder aplicar la funcion de borrar puntos
+var arr_Img = new Array(); //arreglo donde guardo los estados de la imagen para poder aplicar la funcion de borrar puntos
 var posImgCv = -1; //una variable tipo "contador" para saber en que posion ando, lo ocupo para restablecer la img cuando borro puntos
-var arrayX = []; //variable donde almaceno las posiciones de X
-var arrayY = []; //variable donde almaceno las posiciones de Y
+var arrayX = []; //arreglo donde almaceno las posiciones de X
+var arrayY = []; //arreglo donde almaceno las posiciones de Y
 /* Este evento controla la forma de abrir un archivo mediante el evento de arrastrar y soltar */
 function handleDragOver(evt) {
     evt.stopPropagation();
@@ -30,6 +30,9 @@ var imgLocal = new ImageLocal(pantalla1);
 imgLocal.getImage().onload = imgLocal.onload;
 //var imgLocal4: ImageLocal = new ImageLocal(pantalla4);
 //imgLocal4.getImage().onload = imgLocal4.onload;
+function dibujarPuntos() {
+    lienzo1.addEventListener("mousedown", puntosRec);
+}
 function puntosRec(evt) {
     var posicionX = evt.offsetX;
     var posicionY = evt.offsetY;
@@ -42,34 +45,33 @@ function puntosRec(evt) {
     arrayX.push(posicionX);
     arrayY.push(posicionY);
 }
-function dibujarPuntos() {
-    lienzo1.addEventListener("mousedown", puntosRec);
+function borrarCanvas() {
+    posImgCv--;
+    arr_Img.pop();
+    pantalla1.putImageData(arr_Img[posImgCv], 0, 0);
 }
 document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
 //document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', imgLocal.handleFileSelect, false);
-function restableceImg() {
-    if (!arrayX.length || !arrayY.length || posImgCv < 3) { //comprobamos si el arreglo de puntos esta vacio
-        alert("Seleccionar los puntos X y Y de la región a ampliar");
-    }
-    else {
-        pantalla1.putImageData(arr_Img[0], 0, 0);
-    }
-}
 function bilinealImg(evt) {
     if (!arrayX.length || !arrayY.length || posImgCv < 3) { //comprobamos si el arreglo de puntos esta vacio
         alert("Seleccionar los puntos X y Y de la región a ampliar");
     }
     else {
+        pantalla1.putImageData(arr_Img[0], 0, 0); /*vuelvo a dibujar la imagen en el canvas porque
+        como estoy dibujando puntos negros a la hora de la transfomacion esos puntos se quedan en la imagen de salida*/
         var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
         imagenSal.imageArray2DtoData(pantalla2, MathImg.bilineal(imagenSal, arrayX, arrayY));
     }
 }
-function borrarCanvas() {
-    posImgCv--;
-    arr_Img.pop();
-    pantalla1.putImageData(arr_Img[posImgCv], 0, 0);
+function restableceImg() {
+    if (!arrayX.length || !arrayY.length || posImgCv < 3) { //comprobamos si el arreglo de puntos esta vacio
+        alert("No se han dibujado puntos en la imagen");
+    }
+    else {
+        pantalla1.putImageData(arr_Img[0], 0, 0);
+    }
 }
 function limpiarCanvas() {
     pantalla1.clearRect(0, 0, DefaultSettings.SIZE_WIDTH, DefaultSettings.SIZE_HEIGHT);
--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,10 @@ let pantalla1: CanvasRenderingContext2D;
 let pantalla2: CanvasRenderingContext2D;
 
 
-let arr_Img = new Array();//contador donde guardo los estados de la imagen para poder aplicar la funcion de borrar puntos
+let arr_Img = new Array();//arreglo donde guardo los estados de la imagen para poder aplicar la funcion de borrar puntos
 let posImgCv = -1;//una variable tipo "contador" para saber en que posion ando, lo ocupo para restablecer la img cuando borro puntos
-let arrayX : number []=[];//variable donde almaceno las posiciones de X
-let arrayY : number []=[];//variable donde almaceno las posiciones de Y
+let arrayX : number []=[];//arreglo donde almaceno las posiciones de X
+let arrayY : number []=[];//arreglo donde almaceno las posiciones de Y
 
 /* Este evento controla la forma de abrir un archivo mediante el evento de arrastrar y soltar */
 function handleDragOver(evt:any) {
@@ -39,6 +39,9 @@ imgLocal.getImage().onload = imgLocal.onload;
 //imgLocal4.getImage().onload = imgLocal4.onload;
 
 
+function dibujarPuntos(){
+  lienzo1.addEventListener("mousedown", puntosRec);
+}
 
 function puntosRec(evt: any): void{
   let posicionX:number = evt.offsetX;
@@ -53,37 +56,39 @@ function puntosRec(evt: any): void{
   arrayX.push(posicionX);
   arrayY.push(posicionY);
 }
-
-function dibujarPuntos(){
-  lienzo1.addEventListener("mousedown", puntosRec);
-}
-document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
-//document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', imgLocal.handleFileSelect, false);
-
-function restableceImg(){
-  if(!arrayX.length || !arrayY.length || posImgCv < 3 ){//comprobamos si el arreglo de puntos esta vacio
-    alert("Seleccionar los puntos X y Y de la región a ampliar");
-   }else{
-    pantalla1.putImageData(arr_Img[0], 0, 0);
-   }
-}
-function bilinealImg(evt: any): void{
-   if(!arrayX.length || !arrayY.length || posImgCv < 3 ){//comprobamos si el arreglo de puntos esta vacio
-    alert("Seleccionar los puntos X y Y de la región a ampliar");
-    
-   }else{
-    var imagenSal: ImageType = new ImageType(pantalla1, imgLocal.getImage());
-    imagenSal.imageArray2DtoData(pantalla2, MathImg.bilineal(imagenSal, arrayX, arrayY));
-
-  }
- }
 function borrarCanvas(){
   posImgCv --;
   arr_Img.pop();
   pantalla1.putImageData(arr_Img[posImgCv], 0, 0);
 }
+
+document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
+//document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', imgLocal.handleFileSelect, false);
+
+
+function bilinealImg(evt: any): void{
+   if(!arrayX.length || !arrayY.length || posImgCv < 3 ){//comprobamos si el arreglo de puntos esta vacio
+    alert("Seleccionar los puntos X y Y de la región a ampliar");
+    
+   }else{
+    pantalla1.putImageData(arr_Img[0], 0, 0);/*vuelvo a dibujar la imagen en el canvas porque 
+    como estoy dibujando puntos negros a la hora de la transfomacion esos puntos se quedan en la imagen de salida*/
+    var imagenSal: ImageType = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.bilineal(imagenSal, arrayX, arrayY));
+
+  }
+ }
+
+ function restableceImg(){
+  if(!arrayX.length || !arrayY.length || posImgCv < 3 ){//comprobamos si el arreglo de puntos esta vacio
+    alert("No se han dibujado puntos en la imagen");
+   }else{
+    pantalla1.putImageData(arr_Img[0], 0, 0);
+   }
+}
+
 function limpiarCanvas(){
   pantalla1.clearRect(0, 0, DefaultSettings.SIZE_WIDTH, DefaultSettings.SIZE_HEIGHT);
   pantalla2.clearRect(0, 0, DefaultSettings.SIZE_WIDTH, DefaultSettings.SIZE_HEIGHT);
